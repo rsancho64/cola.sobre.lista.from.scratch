28e0816cc52020f346fd473ca54aabebe179311e
rsancho64 <rsancho@iesrioarba.es>
2024-10-16 12:28:13 +0200

    .

--- cola.py
+++ cola.py
@@ -1,1 +1,44 @@
 #! /usr/bin/python3
+
+class cola:
+
+    __data = []
+    __size = None
+
+    def __init__(self, size):
+        self.__size = size
+
+    def h(self) -> int: 
+        return len(self.__data)
+
+    def __str__(self) -> str:
+        answ = "<"
+        answ += f"{self.h()} de {self.__size}, {self.__data}"
+        # if self.esVacia(): answ += "VACIA"
+        # if self.esLlena(): answ += "LLENA"
+        answ += ">"
+        return answ
+
+    def esVacia(self) -> bool:
+        pass
+
+    def esLlena(self) -> bool:
+        return False
+    
+    def enqueue(self, something):
+        if not self.esLlena():
+            self.__data.append(something)
+
+    # def dqueue() -> object:
+    #     pass
+
+if __name__ == "__main__":
+
+    c = cola(4)
+    print(c)
+
+    c.enqueue(11)
+    print(c)
+
+    c.enqueue(22)
+    print(c)    
