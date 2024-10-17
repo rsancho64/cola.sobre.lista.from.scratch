f4d3095625ab46ea9474e6ec12a84fe34b153e82
rsancho64 <rsancho@iesrioarba.es>
2024-10-17 12:35:41 +0200

    .

--- cola.py
+++ cola.py
@@ -3,31 +3,33 @@
 class cola:
 
     __data = []
-    __size = None
+    __maxSize = None  
 
     def __init__(self, size):
-        self.__size = size
+        self.__maxSize = size
 
-    def h(self) -> int: 
+    def len(self) -> int: 
         return len(self.__data)
 
     def __str__(self) -> str:
         answ = "<"
-        answ += f"{self.h()} de {self.__size}, {self.__data}"
-        # if self.esVacia(): answ += "VACIA"
-        # if self.esLlena(): answ += "LLENA"
+        answ += f"{self.len()} de {self.__maxSize}, {self.__data}"
+        if self.esVacia(): answ += "VACIA"
+        if self.esLlena(): answ += "LLENA"
         answ += ">"
         return answ
 
     def esVacia(self) -> bool:
-        pass
+        return len(self.__data) == 0
 
     def esLlena(self) -> bool:
-        return False
+        return len(self.__data) == self.__maxSize
     
     def enqueue(self, something):
         if not self.esLlena():
             self.__data.append(something)
+        else:
+            raise OverflowError (f'queue: ya estoy llena')
 
     # def dqueue() -> object:
     #     pass
@@ -37,8 +39,23 @@ if __name__ == "__main__":
     c = cola(4)
     print(c)
 
-    c.enqueue(11)
-    print(c)
+    try:
+        c.enqueue(11)
+        print(c)
 
-    c.enqueue(22)
-    print(c)    
+        c.enqueue(22)
+        print(c)    
+
+        c.enqueue(33)
+        print(c)
+
+        c.enqueue(44)
+        print(c)
+
+        c.enqueue(55)
+        print(c)
+
+    except OverflowError:
+        print("no entro, estaba llena")
+
+    print(c)
